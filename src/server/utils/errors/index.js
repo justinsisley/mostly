26adc3ddb5a1ca2ee46f3e03bd94e00e3e376455
Justin Sisley <jsisley@paypal.com>
2018-06-07 10:23:09 -0700

    Updated comments in server error util

--- src/server/utils/errors/index.js
+++ src/server/utils/errors/index.js
@@ -1,11 +1,11 @@
-// Wrapper function for route handlers that automatically catches errors
+// Wrapper function for route handlers that catches any uncaught errors
 export function catchErrors(fn) {
   return (req, res, next) => {
     Promise.resolve(fn(req, res, next)).catch(next);
   };
 }
 
-// Middleware for handling uncaught errors
+// Middleware for handling the response for uncaught errors
 // eslint-disable-next-line no-unused-vars
 export function handleErrors(err, req, res, next) {
   if (err && err.stack) console.error(err.stack);
