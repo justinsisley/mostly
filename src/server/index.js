544ba98afb7c3d195adab8bf49e1aaa0eccedc23
Justin Sisley <jsisley@paypal.com>
2018-06-07 10:41:07 -0700

    Updated comments in server index

--- src/server/index.js
+++ src/server/index.js
@@ -10,15 +10,15 @@ import hot from './utils/hot';
 const app = express();
 
 // Middleware
-app.use(helmet());
-app.use(morgan(isProd ? 'common' : 'dev'));
-app.use(express.json());
+app.use(helmet()); // Basic security for Express
+app.use(morgan(isProd ? 'common' : 'dev')); // Basic logging
+app.use(express.json()); // Parse JSON in request bodies
 
 if (isProd) {
-  app.use('/api', api);
-  app.use(express.static('dist/client'));
+  app.use('/api', api); // Handle all "/api" paths
+  app.use(express.static('dist/client')); // Serve static assets for the client
 
-  // Send index.html for all unhandled non-API requests
+  // Send index.html for all unhandled requests
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/index.html'));
   });
